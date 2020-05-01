fdbea88cdb702a0ba36f0f153f90f34fa62abbfc
Josiah Dunham <josiah.dunham@Josiahs-MacBook-Air.local>
2020-05-01 07:38:46 -0400

    missed saving a file

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -25,8 +25,6 @@ export const typeChecks = {
     hasValue
 }
 
-const alpha = "abcdefghijklmnopqrstuvwxyz"
-
 export const getRandomInteger = (max: number, min = 0) => Math.floor(Math.random() * (max - min)) + min
 
 export const getRandomElelmentFromArray = (items: string[]) => items[getRandomInteger(items.length)]
